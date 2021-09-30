ef57852b0276b8cd0d37d0a852acc435522870a7
Bobby2006 <89638487+Bobby2006@users.noreply.github.com>
2021-09-30 21:32:52 +1000

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,24 @@
+class Ground
+{
+    constructor(posX, posY, width, height)
+    {
+        this.x = posX;
+        this.y = posY;
+        this.w = width;
+        this.h = height;
+
+        var options =
+        {
+            isStatic: true
+        }
+
+        this.ground = Bodies.rectangle(posX, posY, width, height, options);
+        World.add(myworld, this.ground);
+    }
+
+    display()
+    {
+        rectMode(CENTER);
+        rect(this.posX, this.posY, this.width, this.height);
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,66 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var ball;
+
+var myengine, myworld;
+
+var groundObj;
+
+var leftSide, rightSide;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+
+	var ball_options =
+	{
+		isStatic: false,
+		restitution: 0.3,
+		friction: 0,
+		density: 1.2
+	}
+
+	myengine = Engine.create();
+	myworld = myengine.world;
+
+	//Create the Bodies Here.
+	ball = Bodies.circle(50, 50, 5, ball_options);
+	World.add(ball);
+
+	Engine.run(myengine);
+
+	groundObj = new Ground(width/2, height-50, width, 10);
+
+	rightSide = new Ground(width-120, ground-20, 5, 120);
+	leftside = new Ground(rightSide-80, ground-20, 5, 120);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  groundObj.display();
+
+  rightSide.display();
+  leftside.display();
+
+  drawSprites();
+
+  keyPressed();
+}
+
+function keyPressed()
+{
+	if(keyCode == UP_ARROW)
+	{
+		Body.applyForce(ball, {x:-6,y:0}, {x:0,y:0});
+	}
+}
